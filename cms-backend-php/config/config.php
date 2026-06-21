460078d664188edd276a20a41247cd87cb2b85f2
WakyUp <nikemag1234@gmail.com>
2026-06-21 11:51:40 +0500

    Update config.php

--- cms-backend-php/config/config.php
+++ cms-backend-php/config/config.php
@@ -3,14 +3,15 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Dotenv\Dotenv;
 
-$dotenv = Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 // CORS заголовки
-header("Access-Control-Allow-Origin: *");
+$allowed_origin = $_ENV['ALLOWED_ORIGIN'] ?? '*';
+header("Access-Control-Allow-Origin: $allowed_origin");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
